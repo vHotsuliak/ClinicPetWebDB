49e2eaaf2b721072200072fa4a93c6fbe3d2b954
Vasyl Hotsuliak <vasyl.hotsuliak@gmail.com>
2018-08-25 23:17:37 +0300

    HibernateStorage

--- src/main/java/com/store/HibernateStorage.java
+++ src/main/java/com/store/HibernateStorage.java
@@ -4,23 +4,29 @@ import com.models.Client;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.Collection;
 
 public class HibernateStorage implements Storage {
-    private final SessionFactory factory;
+    private final EntityManagerFactory entityManagerFactory;
 
     public HibernateStorage() {
-        try {
-            factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+        entityManagerFactory = Persistence.createEntityManagerFactory("clinicHibernate");
     }
 
     @Override
     public Collection<Client> values() {
-       return null;
+       EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+       entityManager.getTransaction().begin();
+       try {
+           return entityManager.createQuery("from Client join Pet on Client.id = Pet.ownerID").getResultList();
+       }finally {
+           entityManager.getTransaction().commit();
+           entityManager.close();
+       }
     }
 
     @Override
@@ -30,6 +36,15 @@ public class HibernateStorage implements Storage {
 
     @Override
     public void add(Client client) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(client);
+            entityManager.persist(client.getPet());
+        }finally {
+            entityManager.getTransaction().commit();
+            entityManager.close();
+        }
 
     }
 
